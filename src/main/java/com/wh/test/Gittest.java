e3ed814ae8c9141a6b122eec6123144d24d5e048
MACK <MACK.com>
2022-04-05 09:46:23 +0800

    ideatest2

--- src/main/java/com/wh/test/Gittest.java
+++ src/main/java/com/wh/test/Gittest.java
@@ -7,5 +7,8 @@ package com.wh.test;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+
     }
 }
