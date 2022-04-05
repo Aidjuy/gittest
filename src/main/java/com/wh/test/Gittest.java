e556a804dfce317fc2090dd7cf9bc164c6dbfde6
MACK <MACK.com>
2022-04-05 09:43:08 +0800

    ideatest

---
+++ src/main/java/com/wh/test/Gittest.java
@@ -0,0 +1,11 @@
+package com.wh.test;
+
+/**
+ * @author 汪航~
+ * @version 1.0
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
